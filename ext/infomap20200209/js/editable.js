377d8067d72eef1793fb7a1c9d268ba26b86c690
hohohohoyosuke <hohohoho.yosuke@gmail.com>
2020-02-15 17:43:42 +0900

    リファクタ

--- ext/infomap20200209/js/editable.js
+++ ext/infomap20200209/js/editable.js
@@ -20,15 +20,30 @@ let addContents = "";
 
 //localStorageにコンテンツの内容が保存されているか確認をして、処理を分岐させることでエラーを回避
 if(localStorage.getItem("editableContent")){
-    //localStorageから、保存したコンテンツ(キーはeditableContent)を取得し、変数addContentsに追加します。
+    //localStorageから、保存したコンテンツ(キーはeditableContent)を取得し、変数addContentsに追加
     addContents += localStorage.getItem("editableContent");
 } else {
-    //localStorageに保存したコンテンツがない場合は、デフォルトの内容を変数addContentsに追加します。
-    //元記述
-    //addContents += '<div id="editableContent" contenteditable="true" onInput="onInput();">編集可能コンテンツ</div>';
-    //addボタンとかもいれた
-    addContents += '<div id="editableContent" contenteditable="true" oninput="onInput();"><div class="category_outer"><div class="category"><div class="category_name"><p>情報<br>デザイン</p></div></div><div class="link"><div class="link_name"><p><i src="./img/"></i><a href="https://infodesign.amebaownd.com/" target="_blank">INFO DESIGN</a></p></div></div></div><div contenteditable="false"><div id="addlinkbutton" class="addlinkbutton">Add<br>link</div><div id="addcategorybutton" class="addcategorybutton">Add<br>Caterogy</div></div></div>';
-
+    //localStorageに保存したコンテンツがない場合は、デフォルトの内容を変数addContentsに追加
+    addContents += '\
+    <div id="editableContent" contenteditable="true" oninput="onInput();">\
+      <div class="category_outer">\
+        <div class="category">\
+          <div class="category_name"><p>情報<br>デザイン</p>\
+          </div>\
+        </div>\
+        <div class="link">\
+          <div class="link_name"><p><i src="./img/"></i><a href="https://infodesign.amebaownd.com/" target="_blank">INFO DESIGN</a></p>\
+          </div>\
+        </div>\
+      </div>\
+      <div contenteditable="false">\
+        <div id="addlinkbutton" class="addlinkbutton">Add<br>link\
+        </div>\
+        <div id="addcategorybutton" class="addcategorybutton">Add<br>Caterogy\
+        </div>\
+      </div>\
+    </div>\
+    ';
 }
 
 //最後に変数addContentsの内容をid"wrapper"の中身に追加
