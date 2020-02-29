14e15b651c04dbea7cfd5f1a13073bd540d31cf1
hohohohoyosuke <hohohoho.yosuke@gmail.com>
2020-02-29 15:17:18 +0900

    tabのURL取得できた！

--- ext/infomap20200209/js/editable.js
+++ ext/infomap20200209/js/editable.js
@@ -74,109 +74,119 @@ window.onload = function() {
     var faketitle = document.createTextNode('都内で朝早くからやってる本屋');
     //タブの情報を取得する
     //参考：https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/manifest.json/permissions
+    //console.log(Tab.title);
+    //var tabTitle = document.createTextNode(Tab.title);
 
-    console.log(Tab.title);
-    var tabTitle = document.createTextNode(Tab.title);
+    //参考：http://www.inashiro.com/2011/06/13/dev-chrome-extension-get-current-tab-url/
+    chrome.tabs.getSelected(window.id, function (tab) {
+      //tab.urlに開いているタブのURLが入っている
+      var url2 = document.createTextNode(tab.url);
+      console.log(tab.url);
+      //document.getElementById('url').appendChild(url); 
+    
+  
 
-    //円表現のクラスを付与
-    //link.classList.add('linkcircle');
-    //link.classList.add('c5');
-    //リンク文字列用のクラスを付与
-    link.classList.add('linktext');
-    //ドラッグアンドドロップのためのクラスを付与
-    link.classList.add('drag-and-drop');
-    //ドラッグのクラスを付与
-    link.classList.add('drag');
-    /*localstorageに保存する関数が発動できるように、idを振る*/
-    link.setAttribute('id', 'editableContent');
-    link.addEventListener('click', function(){
-      link.contentEditable='true';
-      });
-    //editableContent>link直下にtitleを追加する
-    document.getElementById('editableContent').appendChild(link).appendChild(faketitle);
-    //urlを描画する
-    //document.getElementById('editableContent').appendChild(link).appendChild(url);
 
-    //追加した要素に対し、ドラッグアンドドロップできるようにする
-    (function(){
+      //円表現のクラスを付与
+      //link.classList.add('linkcircle');
+      //link.classList.add('c5');
+      //リンク文字列用のクラスを付与
+      link.classList.add('linktext');
+      //ドラッグアンドドロップのためのクラスを付与
+      link.classList.add('drag-and-drop');
+      //ドラッグのクラスを付与
+      link.classList.add('drag');
+      /*localstorageに保存する関数が発動できるように、idを振る*/
+      link.setAttribute('id', 'editableContent');
+      link.addEventListener('click', function(){
+        link.contentEditable='true';
+        });
+      //editableContent>link直下にtitleを追加する
+      document.getElementById('editableContent').appendChild(link).appendChild(url2);
+      //urlを描画する
+      //document.getElementById('editableContent').appendChild(link).appendChild(url);
+    
+      //追加した要素に対し、ドラッグアンドドロップできるようにする
+      (function(){
 
-      // 要素の取得
-      var elements = document.getElementsByClassName("drag-and-drop");
+        // 要素の取得
+        var elements = document.getElementsByClassName("drag-and-drop");
 
-      //要素内のクリックされた位置を取得するグローバル（のような）変数
-      var x;
-      var y;
+        //要素内のクリックされた位置を取得するグローバル（のような）変数
+        var x;
+        var y;
 
-      //マウスが要素内で押されたとき、又はタッチされたとき発火
-      for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
-      }
+        //マウスが要素内で押されたとき、又はタッチされたとき発火
+        for(var i = 0; i < elements.length; i++) {
+          elements[i].addEventListener("mousedown", mdown, false);
+          elements[i].addEventListener("touchstart", mdown, false);
+        }
 
-    //マウスが押された際の関数
-    function mdown(e) {
+      //マウスが押された際の関数
+      function mdown(e) {
 
-      //クラス名に .drag を追加
-      this.classList.add("drag");
+        //クラス名に .drag を追加
+        this.classList.add("drag");
 
-      //タッチイベントとマウスのイベントの差異を吸収
-      if(e.type === "mousedown") {
-        var event = e;
-      } else {
-        var event = e.changedTouches[0];
-      }
-
-      //要素内の相対座標を取得
-      x = event.pageX - this.offsetLeft;
-      y = event.pageY - this.offsetTop;
-
-      //ムーブイベントにコールバック
-      document.body.addEventListener("mousemove", mmove, false);
-      document.body.addEventListener("touchmove", mmove, false);
-      }
-
-      //マウスカーソルが動いたときに発火
-      function mmove(e) {
-
-        //ドラッグしている要素を取得
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //同様にマウスとタッチの差異を吸収
-        if(e.type === "mousemove") {
+        //タッチイベントとマウスのイベントの差異を吸収
+        if(e.type === "mousedown") {
           var event = e;
         } else {
           var event = e.changedTouches[0];
         }
 
-        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
-        e.preventDefault();
+        //要素内の相対座標を取得
+        x = event.pageX - this.offsetLeft;
+        y = event.pageY - this.offsetTop;
 
-        //マウスが動いた場所に要素を動かす
-        drag.style.top = event.pageY - y + "px";
-        drag.style.left = event.pageX - x + "px";
+        //ムーブイベントにコールバック
+        document.body.addEventListener("mousemove", mmove, false);
+        document.body.addEventListener("touchmove", mmove, false);
+        }
 
-        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
-        drag.addEventListener("mouseup", mup, false);
-        document.body.addEventListener("mouseleave", mup, false);
-        drag.addEventListener("touchend", mup, false);
-        document.body.addEventListener("touchleave", mup, false);
+        //マウスカーソルが動いたときに発火
+        function mmove(e) {
 
-      }
+          //ドラッグしている要素を取得
+          var drag = document.getElementsByClassName("drag")[0];
 
-      //マウスボタンが上がったら発火
-      function mup(e) {
-        var drag = document.getElementsByClassName("drag")[0];
+          //同様にマウスとタッチの差異を吸収
+          if(e.type === "mousemove") {
+            var event = e;
+          } else {
+            var event = e.changedTouches[0];
+          }
 
-        //ムーブベントハンドラの消去
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
+          //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+          e.preventDefault();
 
-        //クラス名 .drag も消す
-        drag.classList.remove("drag");
-      }
-    })()
+          //マウスが動いた場所に要素を動かす
+          drag.style.top = event.pageY - y + "px";
+          drag.style.left = event.pageX - x + "px";
+
+          //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+          drag.addEventListener("mouseup", mup, false);
+          document.body.addEventListener("mouseleave", mup, false);
+          drag.addEventListener("touchend", mup, false);
+          document.body.addEventListener("touchleave", mup, false);
+
+        }
+
+        //マウスボタンが上がったら発火
+        function mup(e) {
+          var drag = document.getElementsByClassName("drag")[0];
+
+          //ムーブベントハンドラの消去
+          document.body.removeEventListener("mousemove", mmove, false);
+          drag.removeEventListener("mouseup", mup, false);
+          document.body.removeEventListener("touchmove", mmove, false);
+          drag.removeEventListener("touchend", mup, false);
+
+          //クラス名 .drag も消す
+          drag.classList.remove("drag");
+        }
+      })()
+    });
   });//document.getElementById("addlinkbutton").addEventListener終わり
 
   /*カテゴリを追加する*/
